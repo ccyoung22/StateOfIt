c046477f9b2f72c0019be87e3b8ac1510d0f1602
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-02-20 18:34:02 +0000

    Scrolling bar logo fixed

--- src/FirstThingsFirst/FirstThingsFirst.jsx
+++ src/FirstThingsFirst/FirstThingsFirst.jsx
@@ -3,7 +3,7 @@ import styles from "./FirstThingsFirst.module.css";
 export default function FirstThingsFirst() {
   return (
     <div className={styles.container}>
-      <p className={styles.title}>First Thing's First</p>
+      <p className={styles.title}>The State of The Art Bar</p>
     </div>
   );
 }
